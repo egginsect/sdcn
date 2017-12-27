9ec78f5bd490ea25cb66489a384519c74fa9b5da
Hao-Wei Lee <hwlee@cs.toronto.edu>
2017-12-27 11:35:58 -0500

    add script for combining dataset

---
+++ behavioral_cloning/combine_dataset.sh
@@ -0,0 +1,19 @@
+output=$1
+if [ ! -d $output ]; then
+  echo "$output does not exist"
+  echo "Create output folder: $output"
+  mkdir -p $output/IMG
+fi
+shift
+files=""
+for folder in $@
+do
+  echo $folder
+  files="$files $folder/driving_log.csv" 
+  ls -l $folder/IMG | wc -l
+  find $folder/IMG -name '*.jpg' -exec cp {} $output/IMG \;
+done
+
+echo "files to be combined are $files" 
+echo "files are combined to $output/driving_log.csv"
+cat $files > $output/driving_log.csv
